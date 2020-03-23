a084e0505364ae910bdf6cc403a9b2f5b7f15004
JLiev <lievjorg@gmail.com>
2020-03-23 15:45:03 -0600

    finish routing and navigating module from PS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { EventsListComponent } from './events-list/events-list.component';
-import { EventThumbnailComponent } from './events-list/event-thumbnail.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent
+} from './events-list/index';
 
-import { EventService } from './events-list/shared/event.service';
-import { EventDetailsComponent } from './events-list/event-details/event-details.component';
-import { CreateEventComponent } from './events-list/create-event.component';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { Error404Component } from './errors/404.component';
 
 import { appRoutes } from './routes';
@@ -39,6 +41,9 @@ import { appRoutes } from './routes';
 })
 export class AppModule { }
 
-export function checkDirtyState() {
-
+export function checkDirtyState(component: CreateEventComponent) {
+  if(component.isDirty) {
+    return window.confirm('You have not saved this event, do you really want to cancel?')
+  }
+  return true;
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,11 +4,15 @@ import { Routes } from '@angular/router';
 import { CreateEventComponent } from './events-list/create-event.component';
 import { Error404Component } from './errors/404.component';
 import { EventRouteActivator } from './events-list/event-details/event-route-activator.service';
+import { EventListResolverService } from './events-list/event-list-resolver.service';
 
 export const appRoutes: Routes = [
-  {path: 'events/new', component: CreateEventComponent, canDeactivate: [canDeactivateCreateEvent]},
-  {path: 'events', component: EventsListComponent},
+  {path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent']},
+  {path: 'events', component: EventsListComponent, resolve: {events: EventListResolverService}},
   {path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},
   {path: '404', component: Error404Component},
-  {path: '', redirectTo: '/events', pathMatch: 'full'}
+  {path: '', redirectTo: '/events', pathMatch: 'full'},
+  {path: 'user', loadChildren:'./user/user.module#UserModule'}
 ];
+
+
